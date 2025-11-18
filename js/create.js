892c426e6976147760a970e033bb1f7dd2ec27e2
Mona Lisa <srorngsokcheat53@gmail.com>
2025-11-18 08:02:48 +0700

    Add: Upload File, and localstorage

--- js/create.js
+++ js/create.js
@@ -2,39 +2,73 @@
 
 "use strict"
 
- const base_url = "https://api.escuelajs.co/api/v1"
+const base_url = "https://api.escuelajs.co/api/v1"
 
-  // create product 
-    async function createProduct() {
+// create product 
+async function createProduct() {
 
-         const frmCreate = document.getElementById('frmCreateProduct'); 
+  // function for upload image 
+   async function submitPhoto() {
+            const photo = document.getElementById("images").files[0];
+            const frmData = new FormData();
 
-        frmCreate.addEventListener('submit', async function(e){
+            const image = frmData.append("file", photo);
 
-            const newProduct = {
-            title: document.getElementById('title').value,
-            price: document.getElementById('price').value,
-            description: document.getElementById('description').value,
-            categoryId:parseInt(document.getElementById('category').value),
-            images: ["https://api.escuelajs.co/api/v1/files/c34b.png"]
-            }
+            const base_url = await fetch("https://api.escuelajs.co/api/v1/files/upload", {
+                method: "POST",
+                body: image
+            });
 
-            e.preventDefault();
+            const data = await base_url.json();
+            console.log("the phto: ", data);
 
-            const res = await fetch(`${base_url}/products`, {
-            method: "POST", 
-            headers: {
-                "Content-Type": "application/json"
-            }, 
-            body: JSON.stringify(newProduct)
-        });
-        const data = await res.json(); 
-        
-        if(data){
-           console.log("Create product successfully!")
-        }else{
-          console.log("Create product unsuccessfully!")
+            return data.location;
+
         }
-        } )
+
+  const frmCreate = document.getElementById('frmCreateProduct');
+
+  frmCreate.addEventListener('submit', async function (e) {
+    
+    e.preventDefault();
+
+    
+    const images = await submitPhoto();
+    console.log("The value of image", images)
+ 
+
+    const newProduct = {
+      title: document.getElementById('title').value,
+      price: document.getElementById('price').value,
+      description: document.getElementById('description').value,
+      categoryId: parseInt(document.getElementById('category').value),
+      images: [images]
     }
-    createProduct(); 
+
+
+    const res = await fetch(`${base_url}/products`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newProduct)
+    });
+    const data = await res.json();
+
+    if (data) {
+      console.log("Create product successfully!")
+    } else {
+      console.log("Create product unsuccessfully!")
+    }
+  })
+}
+createProduct(); 
+
+localStorage.setItem("accessToken", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30");
+
+console.log("The value of accesstoken: ", localStorage.getItem("accessToken"));
+
+localStorage.removeItem("accessToken");
+
+localStorage.clear();
+
